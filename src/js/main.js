2ea475f3059b0aa73031005a3fd6a1e6836e4d07
JustAri <ariichoua@gmail.com>
2025-12-10 17:21:48 +0100

    ajout du logo poke api et du background

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-    const startBtn = document.getElementById("start-button");
-    const slidePage = document.getElementById("slide-page");
-    startBtn.addEventListener("click", () => {
-        slidePage.style.transform = "translateY(-100%)";
-        slidePage.style.transition = "transform 0.5s ease-in-out";
-    });
- });
+//     const startBtn = document.getElementById("start-button");
+//     const slidePage = document.getElementById("slide-page");
+//     startBtn.addEventListener("click", () => {
+//         slidePage.style.transform = "translateY(-100%)";
+//         slidePage.style.transition = "transform 0.5s ease-in-out";
+//     });
+//  });
