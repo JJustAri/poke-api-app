cc2f7c552d19b42fbf0c45bf8e4e86932ead7076
JustAri <ariichoua@gmail.com>
2025-12-18 14:18:47 +0100

    configuration de webpack terminé, migration du projet vers webpack au lieu de CLI a venir

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", () => {
-
-//     const startBtn = document.getElementById("start-button");
-//     const slidePage = document.getElementById("slide-page");
-//     startBtn.addEventListener("click", () => {
-//         slidePage.style.transform = "translateY(-100%)";
-//         slidePage.style.transition = "transform 0.5s ease-in-out";
-//     });
-//  });
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // nettoie le dist à chaque build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // ton fichier HTML source
+    }),
+  ],
+  devServer: {
+    static: ['./dist','./public'],
+    hot: true,
+    watchFiles: ['src/**/*'],
+    open: true,
+  },
+};
