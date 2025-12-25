f82e91c1dddaaf241c4d2e80bb80d6142d1db179
JustAri <ariichoua@gmail.com>
2025-12-25 17:29:29 +0100

    feat: création du squelette de l'app (zone de message d'erreur, zone d'affichage des cartes)

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,8 @@
     
     export function initmodal() { 
 
-    const my_modal_2 = document.getElementById("my_modal_2");
-    my_modal_2.showModal()
+    const explanationModal = document.getElementById("explain-modal");
+    explanationModal.showModal()
     }
     
     
