0d2b5e07748a1c41a3b71ecc0ee52ba3333cecf7
JustAri <ariichoua@gmail.com>
2026-01-06 17:37:10 +0100

    feat: ajout d'un try catch pour mieux gerer les erreurs, ajout du fetch api pour recuperer toute les cartes puis les filtrer

--- src/js/search.js
+++ src/js/search.js
@@ -1,41 +1,72 @@
-    export function searchInit() {
-const searchInput = document.getElementById('pokemon-input');
-const searchButton = document.getElementById('search-button');
-const cardContainer = document.getElementById('card-container');
 
-searchButton.addEventListener('click', function (e) {
+export async function searchInit() {
+  const searchInput = document.getElementById("pokemon-input");
+  const searchButton = document.getElementById("search-button");
+  const cardContainer = document.getElementById("card-container");
+  let cards = [];
+
+  searchButton.addEventListener("click", async function (e) {
     e.preventDefault();
 
+    const url = `https://api.tcgdex.net/v2/fr/cards`;
+
+    if (cards.length === 0) {
+      cards = await getData(url);
+    }
+
     const pokemonName = searchInput.value.toLowerCase().trim();
-    console.log(pokemonName)
-    const url = `https://api.pokemontcg.io/v2/cards?q=name:${encodeURIComponent(pokemonName)}&pageSize=10`;
-    getData(url);
 
-});
+    const filteredCards = cards.filter(card => 
+    card.name.toLowerCase().includes(pokemonName))
 
+    filteredCards.forEach(card => {
+        createCard(card)
+    });
+  });
 }
 
 async function getData(url) {
 
-    const request = new Request(url, {
-        Headers: {
-            'Authorization':'9370014b-3ffc-4e95-83ae-c7a6699918c2'
-        }
-});
-    const response = await fetch(request);
-    const pokemonData = response.json();
-    console.log(pokemonData);
+  try {
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+    let errorMessage = "Une erreur est survenue";
+    
+    if (response.status === 404) {
+        errorMessage = "Pokemon Introuvable";
+    }
+    if (response.status >= 500) {
+        errorMessage = "Le service est temporairement indisponible 😕";
+    }
+
+      throw new Error(`Erreur HTTP : ${response.status}, ${errorMessage}.`);
+    }
+
+    const cards = await response.json();
+
+    return cards;
+
+  } catch (error) {
+
+    console.error('Erreur API TCGdex :', error);
+    showError(error);
+    return [];
+
+  }
 }
 
-function createCard(response) {
+function createCard(card) {
+  let divCard = document.createElement("div");
+  divCard.classList.add(
+    "bg-amber-800 border-2 grow h-5 max-w-[25%] p-4 m-2 rounded-lg flex flex-col items-center"
+  );
 
-    let divCard = document.createElement('div');
-    divCard.classList.add('bg-amber-800 border-2 grow h-5 max-w-[25%] p-4 m-2 rounded-lg flex flex-col items-center');
+  let imgCard = document.createElement("img");
+  imgCard.src = card
+  imgCard.alt = response.name;
+  imgCard.classList.add("box-border");
 
-    let imgCard = document.createElement('img');
-    imgCard.src = response.images.small;
-    imgCard.alt = response.name;
-    imgCard.classList.add("box-border");
-
-    cardContainer.appendChild(divCard)
+  cardContainer.appendChild(divCard);
 }
